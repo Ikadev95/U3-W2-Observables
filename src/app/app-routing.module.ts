2cb2892ff48aeff45c3814f1d9414e991ba225ec
Ikadev95 <fegughi@gmail.com>
2024-10-16 14:44:30 +0200

    componenti e routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { PreferitiPageComponent } from './components/preferiti-page/preferiti-page.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    redirectTo: '/home',
+    pathMatch:'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'preferiti',
+    component: PreferitiPageComponent
+  },
+  {
+    path:'cart',
+    component: CartPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { PreferitiPageComponent } from './components/preferiti-page/preferiti-page.component';
+import { CartPageComponent } from './components/cart-page/cart-page.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ProductCardComponent,
+    PreferitiPageComponent,
+    CartPageComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
